54f067ef967526e7f8348d9664e72321ae0ccce1
Harsh-Nandal <harshnandal862005@gmail.com>
2025-10-27 15:50:13 +0530

    changing terminal issue

--- pages/api/star-corn.js
+++ pages/api/star-corn.js
@@ -1,5 +1,5 @@
 // /pages/api/start-cron.js
-import "../../../cron/attendanceNotifier.js"; // ✅ Only runs server-side
+import "@/lib/attendanceNotifier.js"; // ✅ Only runs server-side
 
 export default function handler(req, res) {
   res.status(200).json({ message: "✅ Cron jobs scheduled successfully!" });
