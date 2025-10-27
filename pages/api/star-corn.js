54d4d06f57bbcea3346cd5af4229963adf319a5b
Harsh-Nandal <harshnandal862005@gmail.com>
2025-10-27 15:51:27 +0530

    still sovle that issue

--- pages/api/star-corn.js
+++ pages/api/star-corn.js
@@ -1,5 +1,5 @@
 // /pages/api/start-cron.js
-import "@/lib/attendanceNotifier.js"; // ✅ Only runs server-side
+import "../../lib/attendanceNotifier.js";
 
 export default function handler(req, res) {
   res.status(200).json({ message: "✅ Cron jobs scheduled successfully!" });
